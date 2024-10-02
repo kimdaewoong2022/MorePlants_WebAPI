48620713c7fa1f087b8ce283b2a7ab4022f1253a
MoreSoft <daewoong.kim@gmail.com>
2024-10-02 16:51:19 +0900

    2-3. 첫 번째 API 엔드포인트

--- MorePlants_PlantsAPI/Controllers/PlantAPIController.cs
+++ MorePlants_PlantsAPI/Controllers/PlantAPIController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MorePlants_PlantsAPI.Models;
 
 namespace MorePlants_PlantsAPI.Controllers
 {
+
+    // PlantsAPIController 컨트롤러에 라우팅 어트리뷰 추가해서 라우팅 오류 해결 
+    [Route("api/PlantAPI")]
     [ApiController]
     public class PlantAPIController : ControllerBase
     {
         // 여기에 엔드포인트 메서드를 추가할 수 있습니다.
+
+        [HttpGet]
+        public IEnumerable<Plant> GetPlants()
+        {
+            return new List<Plant>
+            {
+                new Plant { Id = 1, Name = "여인초" },
+                new Plant { Id = 2, Name = "뱅갈고무나무" }
+            };
+        }
     }
 }
---
+++ MorePlants_PlantsAPI/Models/Plant.cs
@@ -0,0 +1,8 @@
+﻿namespace MorePlants_PlantsAPI.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

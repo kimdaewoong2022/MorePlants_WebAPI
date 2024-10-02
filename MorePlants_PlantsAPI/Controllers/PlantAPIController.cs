5be8a24c9faad3ea8d771dc690d4cee8e1826f89
MoreSoft <daewoong.kim@gmail.com>
2024-10-02 16:02:14 +0900

    2-2. PlantAPIController 생성

---
+++ MorePlants_PlantsAPI/Controllers/PlantAPIController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MorePlants_PlantsAPI.Controllers
+{
+    [ApiController]
+    public class PlantAPIController : ControllerBase
+    {
+        // 여기에 엔드포인트 메서드를 추가할 수 있습니다.
+    }
+}

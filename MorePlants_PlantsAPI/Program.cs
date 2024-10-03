831b518c926f206e7531b7bbc697fd366bc4585a
MoreSoft <daewoong.kim@gmail.com>
2024-10-03 16:48:09 +0900

    3-9. Http Patch Nuget 패키지 설치

--- MorePlants_PlantsAPI/Program.cs
+++ MorePlants_PlantsAPI/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+//3-9. Http Patch Nuget 패키지 설치
+builder.Services.AddControllers().AddNewtonsoftJson();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
